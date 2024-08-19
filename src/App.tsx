5e15923078b78a7fd5e6ec79af27b95723549e2d
Robert Aron <robert.aron.tx@gmail.com>
2024-08-18 19:51:58 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
+import "./App.css";
+import { useRef, useState } from "react";
+import { Mesh } from "three";
+
+function Box(props: MeshProps) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef<Mesh>(null!);
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((_state, delta) => (meshRef.current.rotation.x += delta));
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(_event) => setActive(!active)}
+      onPointerOver={(_event) => setHover(true)}
+      onPointerOut={(_event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl underline">Hello world!</h1>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
